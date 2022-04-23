a1775a889fa13d5af118e0f34e8400107803cc98
yabinboxes <yabin.monroy.1@gmail.com>
2022-04-23 09:13:46 -0700

    adding axios in autodeploy

--- src/controllers/autodeploy-unit/autodeploy-unit.controller.ts
+++ src/controllers/autodeploy-unit/autodeploy-unit.controller.ts
@@ -3,9 +3,13 @@ import { Request, Response } from 'express'
 import IAutodeployUnit from './autodeploy-unit.interface'
 import IControllerBase from '../../interfaces/IControllerBase.interface'
 
+import axios from "axios";
+
+
 class AutodeployUnitController implements IControllerBase {
     public path = '/autodeploy'
     public router = express.Router()
+    private fs = require('fs');
 
     constructor() {
         this.initRoutes()
@@ -26,12 +30,50 @@ class AutodeployUnitController implements IControllerBase {
 
         // create record in prod/dev units deployed
 
+       
+        // call pulumi gcp-deploy-cloud-run and get url
+        axios.get('https://reqres.in/api/users')
+        .then(response => {
+            console.log(response.data.data);
+            
+            
+            // write url in db_url
+            let lyrics = 'But still I\'m having memories of high speeds when the cops crashed\n' + 
+            'As I laugh, pushin the gas while my Glocks blast\n' + 
+            'We was young and we was dumb but we had heart';
+
+           
+
+            // write in app-url
+            this.createFile('urls/app-url', lyrics);
+
+            // write in db-url
+            this.createFile('urls/db-url', lyrics);
 
 
-        return res.json({
+            return res.json(response.data.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+       
+        
+       
+
+        /*return res.json({
             'urlList': [  
                 {'date': 'Jan 1, 2016', 'url': "http://test1"}
             ]
+        });*/
+    }
+
+    createFile(path: string, urlStr: string) {
+
+        this.fs.writeFile(path, urlStr,  function(err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("File created!");
         });
     }
 }
